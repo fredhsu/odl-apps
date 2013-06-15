683dcd7a3e5fadfd6f3409a965e37c979d009100
Fred Hsu <fredlhsu@gmail.com>
2013-06-15 09:05:29 -0700

    Staring new app to grab packets

---
+++ getpackets/src/main/java/com/example/getpackets/Activator.java
@@ -0,0 +1,32 @@
+package com.example.getpackets;
+
+
+import org.apache.felix.dm.Component;
+import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator extends ComponentActivatorAbstractBase {
+    protected static final Logger logger = LoggerFactory
+            .getLogger(Activator.class);
+
+    public void init() {
+    }
+
+    public void destroy() {
+    }
+
+    public Object[] getImplementations() {
+        Object[] res = { GetPackets.class };
+        return res;
+    }
+
+    public void configureInstance(Component c, Object imp, String containerName) {
+        if (imp.equals(GetPackets.class)) {
+            // export the service
+            c.setInterface(new String[] { GetPackets.class.getName() },
+                    null);
+
+        }
+    }
+}

97c510b87194505b64326fe5eccf68e33a74b88f
Fred Hsu <fredlhsu@gmail.com>
2013-06-19 21:20:25 -0700

    Decoding packet, but not sure how to interpret ICMP

--- getpackets/src/main/java/com/example/getpackets/GetPackets.java
+++ getpackets/src/main/java/com/example/getpackets/GetPackets.java
@@ -2,13 +2,16 @@ package com.example.getpackets;
 
 import org.opendaylight.controller.sal.packet.*;
 import org.opendaylight.controller.sal.utils.NetUtils;
+import java.net.InetAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Simple bundle to grab some statistics
  * Fred Hsu
+ * install file:///home/fhsu/code/odl-apps/getpackets/target/getpackets-1.4.0-SNAPSHOT.jar
  */
+
 public class GetPackets implements IListenDataPacket {
     private static final Logger log = LoggerFactory
             .getLogger(GetPackets.class);
@@ -40,18 +43,15 @@ public class GetPackets implements IListenDataPacket {
 
     @Override
     public PacketResult receiveDataPacket(RawPacket inPkt) {
-    System.out.println("recevieDataPacket");
         if (inPkt == null) {
             return PacketResult.IGNORED;
         }
         log.trace("Received a frame of size: {}",
                         inPkt.getPacketData().length);
-        System.out.println("packet size " + inPkt.getPacketData().length);
         Packet formattedPak = this.dataPacketService.decodeDataPacket(inPkt);
         System.out.println("packet");
         System.out.println(formattedPak);
         if (formattedPak instanceof Ethernet) {
-            System.out.println("Ethernet packet");
             System.out.println(formattedPak);
             Object nextPak = formattedPak.getPayload();
             if (nextPak instanceof ICMP) {
@@ -60,23 +60,23 @@ public class GetPackets implements IListenDataPacket {
                 System.out.println(nextPak);
             }
             if (nextPak instanceof IPv4) {
+                IPv4 ipPak = (IPv4)nextPak;
                 System.out.println("IP");
                 log.trace("Handled IP packet");
-                int ipAddr = ((IPv4)nextPak).getSourceAddress();
-                InetAddress ip = NetUtils.getInetAddress(ipAddr.getSourceAddress());
-                //byte[] bytes = BigInteger.valueOf(ipAddr).toByteArray();
-                //InetAddress addr = InetAddress.getByAddress(bytes);
-                System.out.println(ip);
+                int sipAddr = ipPak.getSourceAddress();
+                InetAddress sip = NetUtils.getInetAddress(sipAddr);
+                int dipAddr = ipPak.getDestinationAddress();
+                InetAddress dip = NetUtils.getInetAddress(dipAddr);
+                System.out.println("SRC IP:");
+                System.out.println(sip);
+                System.out.println("DST IP:");
+                System.out.println(dip);
+                if (ipPak.getProtocol() == 1) {
+                    //ICMP icmpPak = (ICMP)ipPak;
+                }
             }
             if (nextPak instanceof ARP) {
-                log.trace("Handled ARP packet");
-                System.out.println("Arp");
-                ARP arp = (ARP)nextPak;
-                System.out.println("Getting protocol address of target:");
-                targetIP = InetAddress.getByAddress(arp.getTargetProtocolAddress());
-                sourceIP = InetAddress.getByAddress(arp.getSenderProtocolAddress());
-
-                System.out.println();
+                //This is handled by the ARP Manager
             }
         }
         return PacketResult.IGNORED;
